9378706379065ed90246b95ed78557ddec93daec
Nikita <thenromanov@gmail.com>
2020-03-25 22:11:10 +0300

    Added choice page

--- mars.py
+++ mars.py
@@ -151,5 +151,29 @@ def selection():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planetName>')
+def planeChoice(planetName):
+    return '''<!DOCTYPE html>
+              <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                    crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                </head>
+                <body>
+                    <h1><b>Моё предложение: {}</b></h1>
+                    <h2 class="p-3 mb-2 bg-primary text-white">Эта планета близка к Земле;</h2>
+                    <h2 class="p-3 mb-2 bg-success text-white">На ней много необходимых ресурсов;</h2>
+                    <h2 class="p-3 mb-2 bg-danger text-white">На ней есть вода и атмосфера</h2>
+                    <h2 class="p-3 mb-2 bg-warning text-dark">На ней есть небольшое магнитное поле;</h2>
+                    <h2 class="p-3 mb-2 bg-info text-white">Наконец, она просто красива!</h2>
+                </body>
+              </html>
+           '''.format(planetName)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
